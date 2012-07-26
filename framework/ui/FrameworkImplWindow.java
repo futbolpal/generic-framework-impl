28148f5db188f51a80942838a9c65bd5d94cff94
futbolpal <futbolpal@gmail.com>
2012-07-25 20:50:01 -0400

    Signed-off-by: futbolpal <futbolpal@gmail.com>

--- framework/ui/FrameworkImplWindow.java
+++ framework/ui/FrameworkImplWindow.java
@@ -13,33 +13,29 @@ import java.awt.image.ImageProducer;
 import java.awt.image.WritableRaster;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 
-public class FrameworkImplWindow extends Window
-{
-  private static FrameworkImplWindow this_;
+public class FrameworkImplWindow extends Window {
+	private static FrameworkImplWindow this_;
 
-  public FrameworkImplWindow()
-  {
-    super();
-    this.setJMenuBar(new FrameworkImplWindowMenuBar());
-    this.addWindowListener(new WindowAdapter()
-    {
-      public void windowClosing(WindowEvent arg0)
-      {
-        System.exit(0);
-      }
-    });
-    
-    
-  }
+	public FrameworkImplWindow() {
+		super();
+		this.setJMenuBar(new FrameworkImplWindowMenuBar());
+		this.setIconImage(new ImageIcon(this.getClass()
+				.getResource("resources/system_icon.png")).getImage());
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent arg0) {
+				System.exit(0);
+			}
+		});
 
-  public static FrameworkImplWindow instance()
-  {
-    if(this_ == null)
-    {
-      this_ = new FrameworkImplWindow();
-    }
-    return this_;
-  }
+	}
+
+	public static FrameworkImplWindow instance() {
+		if (this_ == null) {
+			this_ = new FrameworkImplWindow();
+		}
+		return this_;
+	}
 }
